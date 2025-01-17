f0b9694f309f06d37c2bd868b2e1a2ad7bd65301
oriol <orioltomas79@hotmail.com>
2025-01-17 07:41:30 +0100

    Add vitest

--- swa-calderilla-fe/vite.config.ts
+++ swa-calderilla-fe/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Enables global test functions like 'describe' and 'it'
+    environment: "jsdom", // Simulates a browser-like environment
+    setupFiles: "./vitest.setup.ts", // File for global test setup
+    coverage: {
+      provider: "v8", // Use the 'v8' coverage provider
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
---
+++ swa-calderilla-fe/vitest.setup.ts
@@ -0,0 +1,1 @@
+import '@testing-library/jest-dom'; // Extends expect with DOM matchers
