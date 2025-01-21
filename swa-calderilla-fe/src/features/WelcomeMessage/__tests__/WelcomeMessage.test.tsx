c5bec5f6fbd1ca070e1c274e4e782ffddca8191f
oriol <orioltomas79@hotmail.com>
2025-01-21 07:51:38 +0100

    Add a test

---
+++ swa-calderilla-fe/src/features/WelcomeMessage/__tests__/WelcomeMessage.test.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import LearnMore from "..";
+
+test("renders the LearnMore component correctly", () => {
+  render(<LearnMore />);
+});
