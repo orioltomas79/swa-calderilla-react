1c440342ed265bcac0e1dc9f37eabc7be6f0df1a
oriol <orioltomas79@hotmail.com>
2025-01-30 22:41:38 +0100

    api: Add Calderilla.Api.Tests

---
+++ swa-calderilla-api/Tests/Calderilla.Api.Tests/Functions/Dev/GetNotFoundErrorFunctionShould.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Calderilla.Api.Functions.Dev;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Calderilla.Api.Tests.Functions.Dev
+{
+    public class GetNotFoundErrorFunctionShould
+    {
+        [Fact]
+        public void GetNotFoundErrorFunctionShouldReturn404()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<GetNotFoundErrorFunction>>();
+            var function = new GetNotFoundErrorFunction(loggerMock.Object);
+
+            var httpContext = new DefaultHttpContext();
+            var request = httpContext.Request;
+            request.Method = "GET";
+            request.Body = new MemoryStream(Encoding.UTF8.GetBytes("{}"));
+
+            // Act
+            var result = function.GetNotFoundError(request);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(objectResult.StatusCode, StatusCodes.Status404NotFound);
+        }
+    }
+}
